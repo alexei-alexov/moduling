95bd741139a8902e68bf3ef7c772a7d2869dd436
alexei-alexov <olexii.lykianchyk@gmail.com>
2017-10-23 10:21:01 +0300

    Done working on lab3

--- modeler.py
+++ modeler.py
@@ -2,6 +2,13 @@ from math import log
 import random
 
 
+TABLE_FORMAT = '|{:^3}|{:^6.2f}|{:^6.2f}|{:^6.2f}|{:1}|{:1}|'
+
+
+def round(n, p=2):
+    return float(int(n*(10**p))) / (10**p) or 0.01
+
+
 class Stream(object):
 
     def __init__(self, param, generator):
@@ -13,7 +20,7 @@ class Stream(object):
         self.time = 0
 
     def get(self):
-        d = (1 / param) * log(generator)
+        d = round( - (1 / self.param) * log(round(self.generator.random())))
         self.time += d
         return d
 
@@ -29,17 +36,37 @@ class SingleDeviceSystem(object):
         # reset orders data
         orders = 1
         served_orders = 1
+        serv_start = 0
+        served = True
+        
         # starting time 0
         self.in_stream.reset()
         self.serv_stream.reset()
+
+        # serving first order
+        serv_delta = self.serv_stream.get()
+        service_time = [serv_delta]
         # 
-        while in_stream.time < time:
-            serv_delta = self.serv_stream.get()
+        print('-'*len(TABLE_FORMAT))
+        print(TABLE_FORMAT.format(orders, self.in_stream.time, serv_start, self.serv_stream.time, int(served), int(not served)))
+        
+        while self.in_stream.time < time:
+
+            serv_start = 0
+            served = False
             order_delta = self.in_stream.get()
             orders += 1
+            # print('{} - {}'.format(self.in_stream.time, self.serv_stream.time))
             if self.in_stream.time >= self.serv_stream.time:
-                served_orders +=
-        print("orders: {} served: {}".format(orders, served_orders))
+                served = True
+                self.serv_stream.time = serv_start = self.in_stream.time
+                serv_delta = self.serv_stream.get()
+                served_orders += 1
+                service_time.append(serv_delta)
+
+            print(TABLE_FORMAT.format(orders, self.in_stream.time, serv_start, self.serv_stream.time, int(served), int(not served)))
+        print('Кількість замовлень: {}\nКількість замовлень які отримали обслуговування: {}'. format(orders, served_orders))
+        return (orders, served_orders, service_time)
 
 
 if __name__ == "__main__":
@@ -49,6 +76,37 @@ if __name__ == "__main__":
     iu = float(input("Enter input stream param: "))
     su = float(input("Enter service stream param: "))
 
-    in_s
+    input_stream = Stream(iu, random)
+    service_stream = Stream(su, random)
+
+    system = SingleDeviceSystem(input_stream, service_stream)
+
+    run_results = []
+    service_times = []
+
+    for _ in range(k):
+        run_result = system.test(T)
+        service_times.extend(run_result[2])
+        run_results.append(run_result)
+    
+    served_orders = sum(result[1] for result in run_results)
+    all_orders = sum(result[0] for result in run_results)
+
+    avg_get_served = served_orders / k
+    avg_orders = all_orders / k
+    avg_service_time = sum(service_times) / len(service_times)
+
+    get_served_chance = served_orders / all_orders
+    service_denie_chance = (all_orders - served_orders) / all_orders
+
+    print("------------------------------------\nРезультат тесту\n------------------------------------")
+    print("Кількість тестів: {}".format(k))
+    print("Середнє число вимог: {}".format(avg_orders))
+    print("Середнє число вимог, що отримали обслуговування: {}".format(avg_get_served))
+    print("Середній час обслуговування однієї вимоги: {}".format(avg_service_time))
+    print("Ймовірність обслуговування: {}".format(get_served_chance))
+    print("Ймовірність відмови: {}".format(service_denie_chance))
+    
+
 
         
